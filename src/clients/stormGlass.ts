43604072f75be975f08f069e7729c84c976f812d
viniciusdsouza <vinicius.souza@dock.tech>
2023-05-23 13:40:59 -0300

    [WIP] feat: fetching external api

---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,14 @@
+import { AxiosStatic } from 'axios';
+
+export class StormGlass {
+  readonly stormGlassApiParams =
+    'swellDirection%2CswellHeight%2CswellPeriod%2CwaveDirection%2CwaveHeight%2CwindDirection%2CwindSpeed';
+    readonly stormGlassAPISource = 'noaa'
+  constructor(protected request: AxiosStatic) {}
+  public async fetchPoints(lat: number, lng: number): Promise<{}> {
+    this.request.get(
+      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassApiParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}`
+    );
+    return Promise.resolve({});
+  }
+}
